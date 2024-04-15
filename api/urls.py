71e8993d0225712884ca1cd2ca92f3f9aa7e009a
Abokor-AN <abokor.ahmed.kadar.nour@outlook.com>
2024-04-15 13:07:02 +0300

    Configure auth with basic model

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("signup/", views.signup, name="signup"),
+    path("signin/", views.signin, name="signin"),
+    path("test_token/", views.test_token, name="test_token")
 ]
