86e0894132f8ac51aeb962ce712e9c6b1ff45087
Abokor-AN <abokor.ahmed.kadar.nour@outlook.com>
2024-04-29 11:20:50 +0300

    Update key_generator.py

--- scripts/key_generator.py
+++ scripts/key_generator.py
@@ -1,7 +1,7 @@
 from jwcrypto import jwt, jwk
 
 # Génération des clés
-key = jwk.JWK.generate(kty='RSA', size=2048)
+key = jwk.JWK.generate(kty='RSA', size=3072)
 
 with open('private_key.pem', 'wb') as f:
     f.write(key.export_to_pem(private_key=True,password=None))
